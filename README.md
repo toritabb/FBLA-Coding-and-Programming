71728fc51c320d9d0e9723808007ddf8041245c4
Joe Hopkins <joemhopkins@icloud.com>
2024-03-31 00:49:59 -0600

    Fixed and made all of the comments consistent, made the readme text read from the README.md file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FBLA-Coding-and-Programming
 The HRHS CTE Partners Management System is a Python program built using the Tkinter library.
 This system helps in the management of information related to business and community partners 
-for HRHS's Career and Technical Education department. It provides a user-friendly interface 
+for HRHS"s Career and Technical Education department. It provides a user-friendly interface 
 for viewing partner details, searching for partners, and generating reports in CSV format.
 
 Features:
@@ -10,13 +10,14 @@ Quickly search for partners by name using a dynamic search system.
 Generate reports on partner information and export them in CSV files for backup and ease of use.
 
 Usage:
-Existing Partners - displays current partners' information, allows sorting, and exporting data to CSV.
+Existing Partners - displays current partners" information, allows sorting, and exporting data to CSV.
 Add Partner - provides functionality for adding and saving new partners to the system.
+Delete Partners - deletes all selected partners. To select a partner, click on it. You can select multiple using shift click or control click
 Documentation - describes the modules we used to create our program and the copyright for images used.
 
 Dependencies:
 Requires Python 3.12 installed on your system.
-Install the pillow and requests libraries using 'pip install pillow requests'
+Install the pillow library using "pip install pillow"
 
 
 Program designed in Python 3.12 for FBLA Colorado by Alexander Fiduccia and Joe Hopkins.
--- main.py
+++ main.py
@@ -5,33 +5,12 @@ from PIL import Image, ImageTk
 import csv
 
 # Text for ReadMe
-program_info = """
-The HRHS CTE Partners Management System is a Python program built using the Tkinter library.
-This system helps in the management of information related to business and community partners 
-for HRHS"s Career and Technical Education department. It provides a user-friendly interface 
-for viewing partner details, searching for partners, and generating reports in CSV format.
-
-Features:
-Easily view partner information such as Name, Address, Email, etc.
-Quickly search for partners by name using a dynamic search system.
-Generate reports on partner information and export them in CSV files for backup and ease of use.
-
-Usage:
-Existing Partners - displays current partners" information, allows sorting, and exporting data to CSV.
-Add Partner - provides functionality for adding and saving new partners to the system.
-Delete Partners - deletes all selected partners. To select partners, click on them, you can select multiple using shift click or control click
-Documentation - describes the modules we used to create our program and the copyright for images used.
-
-Dependencies:
-Requires Python 3.12 installed on your system.
-Install the pillow library using "pip install pillow"
+program_info = ""
+with open("README.md") as f:
+    program_info = f.read()[29:]
 
 
-Program designed in Python 3.12 for FBLA Colorado by Alexander Fiduccia and Joe Hopkins.
-"""
-
-
-# Text for Documentation
+# Text for documentation
 library_documentation = """
 Libraries Used:
 
@@ -51,7 +30,7 @@ All images are used under public domain or are original creations.
 """
 
 
-# Display Text Function
+# Function to display a window with the given text
 def display_text_window(text: str) -> None:
     # Create the window
     text_window = tk.Toplevel(root, background=bg_color)
@@ -75,7 +54,7 @@ def display_text_window(text: str) -> None:
     center_window(text_window, text_label.winfo_reqwidth() + 40, text_label.winfo_reqheight() + 86)
 
 
-# Existing Partners Button
+# Function to display the existing partners window
 def display_existing_partners_window() -> None:
     # Create the window
     existing_partners_window = tk.Toplevel(root, background=bg_color)
@@ -88,9 +67,9 @@ def display_existing_partners_window() -> None:
     existing_partners_window.resizable(False, False)
     root.withdraw()
 
-    fieldnames = ["Number", "Name", "Address", "Phone", "Email", "Contact", "Industry"]
-
+    # Function to sort the table by column
     def partner_chart_sort_column(chart: ttk.Treeview, column: str, mode: int) -> None:
+        # Different sort modes
         if mode == 0:
             column_values = [(int(chart.set(child, "Number")), child) for child in chart.get_children()]
             column_values.sort()
@@ -102,10 +81,12 @@ def display_existing_partners_window() -> None:
         for i, (_, k) in enumerate(column_values):
             chart.move(k, '', i)
 
-        # Change mode next time
+        # Change sort mode next time
         chart.heading(column, command=lambda: partner_chart_sort_column(chart, column, (mode + 1) % 3))
 
     # Create a Treeview widget for the chart
+    fieldnames = ["Number", "Name", "Address", "Phone", "Email", "Contact", "Industry"]
+
     style = ttk.Style()
     style.theme_use("clam")
     style.configure("Treeview", background=button_color, bordercolor=border_color)
@@ -130,7 +111,7 @@ def display_existing_partners_window() -> None:
 
     partners_chart.place(x=20, y=20)
 
-    # Insert the partner data into the chart
+    # Function to insert the partner data into the chart
     def update_partner_chart(_) -> None:
         partners_chart.delete(*partners_chart.get_children())
         for partner in partners:
@@ -140,19 +121,20 @@ def display_existing_partners_window() -> None:
     partners_chart.bind("<<Update>>", update_partner_chart)
     partners_chart.event_generate("<<Update>>")
 
-    # Add the Back Button
+    # Add the back button
     back_button = tk.Button(existing_partners_window, text="Back", command=lambda: close_window(existing_partners_window), background=button_color)
     back_button.place(x=20, y=366)
 
-    # Add the Add Partner Button (functionality is marked later)
+    # Add the add partner button (functionality is marked later)
     add_partner_button = tk.Button(existing_partners_window, text="Add Partner", command=lambda: display_add_partner_window(existing_partners_window, partners_chart), background=button_color)
     add_partner_button.place(x=173, y=366)
 
-    # Export Button
+    # Function for the export button
     def export_to_csv() -> None:
-        # Ask for Filename and location
+        # Ask for filename and location
         filename = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
 
+        # Cancel if none is given
         if not filename:
             return
 
@@ -160,22 +142,24 @@ def display_existing_partners_window() -> None:
         with open(filename, "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
 
-            # Write Partner Header Rows
+            # Write partner header rows
             writer.writerow(fieldnames)
 
-            # Write Partner Data Rows
+            # Write partner data rows
             for partner in partners:
                 writer.writerow([partner[field] for field in fieldnames])
 
-    # GUI Export Button
+    # Add export button
     export_button = tk.Button(existing_partners_window, text="Export Chart", command=export_to_csv, background=button_color)
     export_button.place(x=76, y=366)
 
-    # Deletes the selected partners
+    # Function for deleting the selected partners
     def delete_partners() -> None:
+        # Get selected partners
         partner_ids = partners_chart.selection()
 
         if partner_ids:
+            # Delete all of the partners
             def delete() -> None:
                 for id in reversed(partner_ids):
                     partners.pop(partners_chart.index(id))
@@ -185,22 +169,26 @@ def display_existing_partners_window() -> None:
 
                 conformation_window.destroy()
 
+            # Create a window asking the user to confirm if they want to delete
             conformation_window = tk.Toplevel(existing_partners_window, background=bg_color)
             conformation_window.title("Partner Deletion Conformation")
             center_window(conformation_window, 359, 122)
             conformation_window.focus()
             conformation_window.resizable(False, False)
 
+            # Add question text
             question_label = tk.Label(conformation_window, text="Are you sure you want to delete all of the selected partners?\nThis action cannot be undone!")
             question_label.place(x=20, y=20)
 
+            # Add cancel button
             cancel_button = tk.Button(conformation_window, text="Cancel", command=conformation_window.destroy, background=button_color)
             cancel_button.place(x=85.7, y=76)
 
+            # Add confirm button
             confirm_button = tk.Button(conformation_window, text="Confirm", command=delete, background=button_color)
             confirm_button.place(x=208.7, y=76)
 
-    # Delete Partner Button
+    # Add delete partner bButton
     delete_button = tk.Button(existing_partners_window, text="Delete Selected Partners", command=delete_partners, background=button_color)
     delete_button.place(x=267, y=366)
 
@@ -213,7 +201,7 @@ def display_existing_partners_window() -> None:
     search_by_dropdown.config(padx=5, pady=1, width=7, bg=button_color, highlightthickness=0)
     search_by_dropdown.place(x=656, y=369)
 
-    # Add a Search Bar
+    # Add a search bar
     search_label = tk.Label(existing_partners_window, text="Search:", background=bg_color)
     search_label.place(x=760, y=368)
 
@@ -252,7 +240,7 @@ def save_partners() -> None:
             writer.writerow(partner)
 
 
-# All the Buttons and GUI interaction for adding info
+# Function to display the add partner window
 def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.Treeview) -> None:
     add_partner_window = tk.Toplevel(parent_window, background=bg_color)
     add_partner_window.title("Add Partner")
@@ -267,64 +255,70 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
     title_label = tk.Label(add_partner_window, text="Add Partner", font=("Arial", 15, "bold"), background=bg_color)
     title_label.place(x=67.5, y=20)
 
+    # Helper function to add hover text to a label
     def add_hover_text(label: tk.Label, text: str) -> None:
+        # Create the hover text label
         hover_text = tk.Label(add_partner_window, text=text, font=("Arial", 7), background=hover_tip_color)
         label_pos_x = int(label.place_info()["x"]) - (hover_text.winfo_reqwidth() - label.winfo_reqwidth()) * 0.5
         label_pos_y = int(label.place_info()["y"]) - hover_text.winfo_reqheight()
 
+        # Show it when the label is hovered, and hide it otherwise
         label.bind("<Enter>", lambda _: hover_text.place(x=label_pos_x, y=label_pos_y))
         label.bind("<Leave>", lambda _: hover_text.place_forget())
 
-    # User input labels and entry fields
+    # User input labels, entry fields, and hover text
+    
+    # Name
     name_label = tk.Label(add_partner_window, text="Name:", background=bg_color)
     name_label.place(x=106.5, y=70)
     name_entry = tk.Entry(add_partner_window, background=text_box_color)
     name_entry.place(x=65, y=91)
-
     add_hover_text(name_label, "Name must be less than 30 characters long.")
 
+    # Address
     address_label = tk.Label(add_partner_window, text="Address:", background=bg_color)
     address_label.place(x=101.5, y=115)
     address_entry = tk.Entry(add_partner_window, background=text_box_color)
     address_entry.place(x=65, y=136)
-
     add_hover_text(address_label, "Address must start with a street number.")
 
+    # Phone number
     phone_label = tk.Label(add_partner_window, text="Phone:", background=bg_color)
     phone_label.place(x=105.5, y=160)
     phone_entry = tk.Entry(add_partner_window, background=text_box_color)
     phone_entry.place(x=65, y=181)
-
     add_hover_text(phone_label, "Phone must be in the format: +12 (345)-678-9876\nCountry code, parenthesis, and dashes are optional.")
 
+    # Email
     email_label = tk.Label(add_partner_window, text="Email:", background=bg_color)
     email_label.place(x=108, y=205)
     email_entry = tk.Entry(add_partner_window, background=text_box_color)
     email_entry.place(x=65, y=226)
-
     add_hover_text(email_label, "Email must contain an @ and a domain.")
 
+    # Contact information
     contact_label = tk.Label(add_partner_window, text="Contact:", background=bg_color)
     contact_label.place(x=101.5, y=250)
     contact_entry = tk.Entry(add_partner_window, background=text_box_color)
     contact_entry.place(x=65, y=271)
-
     add_hover_text(contact_label, "Contact can only contain letters and spaces.")
 
+    # Industry
     industry_label = tk.Label(add_partner_window, text="Industry:", background=bg_color)
     industry_label.place(x=101, y=295)
     industry_entry = tk.Entry(add_partner_window, background=text_box_color)
     industry_entry.place(x=65, y=316)
-
     add_hover_text(industry_label, "Industry can only contain letters and spaces.")
 
     # Validate the input and then add partner
     def submit_partner(name: str, address: str, phone: str, email: str, contact: str, industry: str) -> None:
+        # Helper function to remove given characters from a string
         def remove_chars(string: str, *chars: str):
             for char in chars:
                 string = string.replace(char, "")
             return string
 
+        # generate all of the validation requirement bools
         raw_phone = remove_chars(phone, "-", " ", "+", "(", ")")
         requirements = [
             len(name) < 30,
@@ -339,10 +333,8 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
 
         # If all of the validation requirements are met, add the partner
         if all(requirements):
-            # Create new partner dictionary
+            # Create new partner dictionary and add it to partners list
             new_partner = {"Number": str(len(partners) + 1), "Name": name, "Address": address, "Phone": phone, "Email": email, "Contact": contact, "Industry": industry}
-
-            # Add new partner to partners list
             partners.append(new_partner)
 
             # Update partner list
@@ -352,8 +344,8 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
             save_partners()
             add_partner_window.destroy()
 
+        # If all requirements aren't met, Issue error pop up box
         else:
-            # Issue error pop up box
             submit_error.place(x=submit_error_pos[0], y=submit_error_pos[1])
 
             root.after(5000, submit_error.place_forget)
@@ -362,6 +354,7 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
     submit_button = tk.Button(add_partner_window, text="Add Partner", command=lambda: submit_partner(name_entry.get(), address_entry.get(), phone_entry.get(), email_entry.get(), contact_entry.get(), industry_entry.get()), background=button_color)
     submit_button.place(x=90, y=356)
 
+    # Create, but don't display, the error button
     submit_error = tk.Label(add_partner_window, text="One or more entered fields are not correct\nTo see syntax rules hover over a field label", font=("Arial", 7), background=hover_tip_color)
     submit_error_pos = (int(submit_button.place_info()["x"]) - (submit_error.winfo_reqwidth() - submit_button.winfo_reqwidth()) * 0.5, int(submit_button.place_info()["y"]) - submit_error.winfo_reqheight())
 
@@ -370,32 +363,21 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
     back_button.place(x=109, y=402)
 
 
-# center the window based on the screen size
+# Helper function to center a window based on the screen size
 def center_window(window: tk.Tk | tk.Toplevel, width: int, height: int) -> None:
     window.geometry('{}x{}+{}+{}'.format(width, height, int(0.5 * (root.winfo_screenwidth() - width)), int(0.5 * (root.winfo_screenheight() - height))))
 
 
-# Close the window and reopen the main window
+# Helper function to close the window and reopen the main window
 def close_window(window: tk.Toplevel) -> None:
     root.deiconify()
     window.destroy()
 
 
-# Define Main-Screen-Button Click Functions
-def button_click(page: str) -> None:
-    match page:
-        case "ReadMe":
-            display_text_window(program_info[1:-1])  # Display basic program information and instructions
-        case "Existing Partners":
-            display_existing_partners_window() # Display current partners and allow for sorting and exporting
-        case "Documentation":
-            display_text_window(library_documentation[1:-1])  # Display library documentation and copyright information
-
-
 # Data for the chart
 partners = []
 
-# Function to read CSV file and populate the partners list
+# Read CSV file and populate the partners list
 with open("partners.csv") as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
@@ -421,11 +403,10 @@ center_window(root, 454, 316)
 root.resizable(False, False)
 
 # Add school logo
-# urllib.request.urlretrieve("https://upload.wikimedia.org/wikipedia/en/2/2f/HRHS_logo.png", "HRHS_Logo.png")
 canvas = tk.Canvas(root, width=100, height=100, highlightthickness=0, background=bg_color)
 canvas.place(x=20, y=20)
 
-image = Image.open("HRHS_Logo.png").resize((100, 100))  # load the image from wikipedia and resize it
+image = Image.open("HRHS_Logo.png").resize((100, 100))
 photo_image = ImageTk.PhotoImage(image, width=100, height=100)
 canvas.create_image(50, 50, image=photo_image)
 
@@ -434,13 +415,13 @@ title_label = tk.Label(root, text="HRHS CTE Partners", font=("Sylfaen", 24, "bol
 title_label.place(x=140, y=20)
 
 # Add buttons
-readme_button = tk.Button(root, text="ReadMe", command=lambda: button_click("ReadMe"), background=button_color)
+readme_button = tk.Button(root, text="ReadMe", command=lambda: display_text_window(program_info[1:-1]), background=button_color)
 readme_button.place(x=20, y=140)
 
-partners_button = tk.Button(root, text="Existing Partners", command=lambda: button_click("Existing Partners"), background=button_color)
+partners_button = tk.Button(root, text="Existing Partners", command=lambda: display_existing_partners_window(), background=button_color)
 partners_button.place(x=20, y=186)
 
-documentation_button = tk.Button(root, text="Documentation", command=lambda: button_click("Documentation"), background=button_color)
+documentation_button = tk.Button(root, text="Documentation", command=lambda: display_text_window(library_documentation[1:-1]), background=button_color)
 documentation_button.place(x=20, y=232)
 
 # Add credits
