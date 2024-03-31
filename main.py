7c983b2e9c1950b4ced4eab0aa55810ac61a2b69
Joe Hopkins <joemhopkins@icloud.com>
2024-03-31 00:27:49 -0600

    Finished coloring everything. Image can't be downloaded from the internet anymore because it needs to be transparent and I couldn't find a transparent one

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import csv
-import requests
-
 
 # Text for ReadMe
 program_info = """
@@ -26,7 +24,7 @@ Documentation - describes the modules we used to create our program and the copy
 
 Dependencies:
 Requires Python 3.12 installed on your system.
-Install the pillow and requests libraries using "pip install pillow requests"
+Install the pillow library using "pip install pillow"
 
 
 Program designed in Python 3.12 for FBLA Colorado by Alexander Fiduccia and Joe Hopkins.
@@ -49,16 +47,14 @@ CSV
 CSV is a simple file format used to store data, such as a spreadsheet or a database.
 We incorporated the CSV library to allow users to export partner information to a file for backup and ease of use.
 
-Requests
-requests is the python library for getting data from websites, like the images we use in out application.
-
 All images are used under public domain or are original creations.
 """
 
 
 # Display Text Function
 def display_text_window(text: str) -> None:
-    text_window = tk.Toplevel(root)  # Store the current window for back button navigation
+    # Create the window
+    text_window = tk.Toplevel(root, background=bg_color)
     text_window.title("Basic Program Information")
 
     # Hide the main window and focus on the new window
@@ -68,11 +64,11 @@ def display_text_window(text: str) -> None:
     root.withdraw()
 
     # Display the text
-    text_label = tk.Label(text_window, text=text)
+    text_label = tk.Label(text_window, text=text, background=bg_color)
     text_label.place(x=20, y=20)
 
     # Add the back button
-    back_button = tk.Button(text_window, text="Back", command=lambda: close_window(text_window))
+    back_button = tk.Button(text_window, text="Back", command=lambda: close_window(text_window), background=button_color)
     back_button.place(x=text_label.winfo_reqwidth() * 0.5 + 2, y=text_label.winfo_reqheight() + 40)
 
     # Center the window
@@ -81,13 +77,9 @@ def display_text_window(text: str) -> None:
 
 # Existing Partners Button
 def display_existing_partners_window() -> None:
-    bg_color = "#4169e1"
-    button_color = "#3454b4"
-    hover_tip_color = "#6787e7"
-    text_box_color = "#b3c3f3"
-
+    # Create the window
     existing_partners_window = tk.Toplevel(root, background=bg_color)
-    existing_partners_window.title("Existing Partners")   # Create a new window
+    existing_partners_window.title("Existing Partners")
 
     # Hide the main window and focus on the existing partners window
     center_window(existing_partners_window, 1012, 412)
@@ -106,28 +98,34 @@ def display_existing_partners_window() -> None:
             column_values = [(chart.set(child, column).lower(), child) for child in chart.get_children()]
             column_values.sort(reverse=bool(mode - 1))
 
-        # rearrange items in sorted positions
+        # Rearrange items in sorted positions
         for i, (_, k) in enumerate(column_values):
             chart.move(k, '', i)
 
-        # change mode next time
-        chart.heading(column, text=column, command=lambda _col=column: partner_chart_sort_column(chart, _col, (mode + 1) % 3))
+        # Change mode next time
+        chart.heading(column, command=lambda: partner_chart_sort_column(chart, column, (mode + 1) % 3))
 
     # Create a Treeview widget for the chart
-    partners_chart = ttk.Treeview(existing_partners_window, columns=("Number", "Name", "Address", "Phone", "Email", "Contact", "Industry"), show="headings", height=15)
-    partners_chart.heading("Number", text="#")
-    partners_chart.heading("Name", text="Name", command=lambda _col="Name": partner_chart_sort_column(partners_chart, _col, 0))
-    partners_chart.heading("Address", text="Address", command=lambda _col="Address": partner_chart_sort_column(partners_chart, _col, 0))
-    partners_chart.heading("Phone", text="Phone", command=lambda _col="Phone": partner_chart_sort_column(partners_chart, _col, 0))
-    partners_chart.heading("Email", text="Email", command=lambda _col="Email": partner_chart_sort_column(partners_chart, _col, 0))
-    partners_chart.heading("Contact", text="Contact", command=lambda _col="Contact": partner_chart_sort_column(partners_chart, _col, 0))
-    partners_chart.heading("Industry", text="Industry", command=lambda _col="Industry": partner_chart_sort_column(partners_chart, _col, 0))
-    partners_chart.column("Number", width=40)
-    partners_chart.column("Name", width=175)
-    partners_chart.column("Address", width=175)
-    partners_chart.column("Phone", width=100)
-    partners_chart.column("Email", width=200)
-    partners_chart.column("Contact", width=120)
+    style = ttk.Style()
+    style.theme_use("clam")
+    style.configure("Treeview", background=button_color, bordercolor=border_color)
+    style.configure("Treeview.Heading", background=button_color, bordercolor=border_color)
+    partners_chart = ttk.Treeview(existing_partners_window, columns=fieldnames, show="headings", height=15)
+
+    partners_chart.heading("Number",   text="#")    # Doesn't need sort
+    partners_chart.heading("Name",     text="Name",     command=lambda: partner_chart_sort_column(partners_chart, "Name",     1))
+    partners_chart.heading("Address",  text="Address",  command=lambda: partner_chart_sort_column(partners_chart, "Address",  1))
+    partners_chart.heading("Phone",    text="Phone",    command=lambda: partner_chart_sort_column(partners_chart, "Phone",    1))
+    partners_chart.heading("Email",    text="Email",    command=lambda: partner_chart_sort_column(partners_chart, "Email",    1))
+    partners_chart.heading("Contact",  text="Contact",  command=lambda: partner_chart_sort_column(partners_chart, "Contact",  1))
+    partners_chart.heading("Industry", text="Industry", command=lambda: partner_chart_sort_column(partners_chart, "Industry", 1))
+
+    partners_chart.column("Number",   width=40)
+    partners_chart.column("Name",     width=175)
+    partners_chart.column("Address",  width=175)
+    partners_chart.column("Phone",    width=100)
+    partners_chart.column("Email",    width=200)
+    partners_chart.column("Contact",  width=120)
     partners_chart.column("Industry", width=160)
 
     partners_chart.place(x=20, y=20)
@@ -138,15 +136,16 @@ def display_existing_partners_window() -> None:
         for partner in partners:
             partners_chart.insert("", tk.END, values=[partner[field] for field in fieldnames])
 
+    # Update the chart and allow it to be updated in the future
     partners_chart.bind("<<Update>>", update_partner_chart)
     partners_chart.event_generate("<<Update>>")
- 
+
     # Add the Back Button
-    back_button = tk.Button(existing_partners_window, text="Back", command=lambda: close_window(existing_partners_window), background=text_box_color)
+    back_button = tk.Button(existing_partners_window, text="Back", command=lambda: close_window(existing_partners_window), background=button_color)
     back_button.place(x=20, y=366)
 
     # Add the Add Partner Button (functionality is marked later)
-    add_partner_button = tk.Button(existing_partners_window, text="Add Partner", command=lambda: display_add_partner_window(existing_partners_window, partners_chart), background=text_box_color)
+    add_partner_button = tk.Button(existing_partners_window, text="Add Partner", command=lambda: display_add_partner_window(existing_partners_window, partners_chart), background=button_color)
     add_partner_button.place(x=173, y=366)
 
     # Export Button
@@ -169,7 +168,7 @@ def display_existing_partners_window() -> None:
                 writer.writerow([partner[field] for field in fieldnames])
 
     # GUI Export Button
-    export_button = tk.Button(existing_partners_window, text="Export Chart", command=export_to_csv, background=text_box_color)
+    export_button = tk.Button(existing_partners_window, text="Export Chart", command=export_to_csv, background=button_color)
     export_button.place(x=76, y=366)
 
     # Deletes the selected partners
@@ -202,7 +201,7 @@ def display_existing_partners_window() -> None:
             confirm_button.place(x=208.7, y=76)
 
     # Delete Partner Button
-    delete_button = tk.Button(existing_partners_window, text="Delete Selected Partners", command=delete_partners, background=text_box_color)
+    delete_button = tk.Button(existing_partners_window, text="Delete Selected Partners", command=delete_partners, background=button_color)
     delete_button.place(x=267, y=366)
 
     # Add search field customization
@@ -211,8 +210,8 @@ def display_existing_partners_window() -> None:
 
     search_by = tk.StringVar(value="Name")
     search_by_dropdown = tk.OptionMenu(existing_partners_window, search_by, *fieldnames[1:], command=lambda _: filter_partners())
-    search_by_dropdown.config(pady=1, width=7, bg=text_box_color)
-    search_by_dropdown.place(x=653, y=366)
+    search_by_dropdown.config(padx=5, pady=1, width=7, bg=button_color, highlightthickness=0)
+    search_by_dropdown.place(x=656, y=369)
 
     # Add a Search Bar
     search_label = tk.Label(existing_partners_window, text="Search:", background=bg_color)
@@ -255,11 +254,6 @@ def save_partners() -> None:
 
 # All the Buttons and GUI interaction for adding info
 def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.Treeview) -> None:
-    bg_color = "#4169e1"
-    button_color = "#3454b4"
-    hover_tip_color = "#6787e7"
-    text_box_color = "#b3c3f3"
-
     add_partner_window = tk.Toplevel(parent_window, background=bg_color)
     add_partner_window.title("Add Partner")
 
@@ -301,7 +295,7 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
     phone_entry = tk.Entry(add_partner_window, background=text_box_color)
     phone_entry.place(x=65, y=181)
 
-    add_hover_text(phone_label, "Phone must be in the format +12 (345)-678-9876\nCountry code, parenthesis, and dashes are optional.")
+    add_hover_text(phone_label, "Phone must be in the format: +12 (345)-678-9876\nCountry code, parenthesis, and dashes are optional.")
 
     email_label = tk.Label(add_partner_window, text="Email:", background=bg_color)
     email_label.place(x=108, y=205)
@@ -357,16 +351,19 @@ def display_add_partner_window(parent_window: tk.Toplevel, partners_chart: ttk.T
             # Save partners to CSV and close page
             save_partners()
             add_partner_window.destroy()
+
         else:
-        #Issue pop up box
-            label = tk.Label(add_partner_window, text="One or more entered fields are not correct\nFor syntax rules hover over a field label", background=text_box_color)
-            label.place(x=13,y=335)
-            label.pack
-            root.after(5000, label.destroy)
+            # Issue error pop up box
+            submit_error.place(x=submit_error_pos[0], y=submit_error_pos[1])
+
+            root.after(5000, submit_error.place_forget)
 
     # Add the Submit Button
     submit_button = tk.Button(add_partner_window, text="Add Partner", command=lambda: submit_partner(name_entry.get(), address_entry.get(), phone_entry.get(), email_entry.get(), contact_entry.get(), industry_entry.get()), background=button_color)
     submit_button.place(x=90, y=356)
+
+    submit_error = tk.Label(add_partner_window, text="One or more entered fields are not correct\nTo see syntax rules hover over a field label", font=("Arial", 7), background=hover_tip_color)
+    submit_error_pos = (int(submit_button.place_info()["x"]) - (submit_error.winfo_reqwidth() - submit_button.winfo_reqwidth()) * 0.5, int(submit_button.place_info()["y"]) - submit_error.winfo_reqheight())
 
     # Add the Back Button
     back_button = tk.Button(add_partner_window, text="Back", command=add_partner_window.destroy, background=button_color)
@@ -409,36 +406,46 @@ with open("partners.csv") as file:
 #  Main Window  #
 #################
 
+# Colors used for everything
+bg_color = "#4169e1"
+button_color = "#b3c3f3"
+hover_tip_color = "#6787e7"
+text_box_color = "#b3c3f3"
+border_color = "#3454b4"
+
 # Create the main window
 root = tk.Tk()
 root.title("HRHS CTE Partners")
-root.configure(background="white")
-center_window(root, 465, 316)
+root.configure(background=bg_color)
+center_window(root, 454, 316)
 root.resizable(False, False)
 
 # Add school logo
-image = Image.open(requests.get("https://upload.wikimedia.org/wikipedia/en/2/2f/HRHS_logo.png", stream=True).raw, formats=["png"]).resize((100, 100))  # load the image from wikipedia and resize it
-photo_image = ImageTk.PhotoImage(image)
-image_label = tk.Label(root, image=photo_image, borderwidth=0)
-image_label.place(x=20, y=20)
+# urllib.request.urlretrieve("https://upload.wikimedia.org/wikipedia/en/2/2f/HRHS_logo.png", "HRHS_Logo.png")
+canvas = tk.Canvas(root, width=100, height=100, highlightthickness=0, background=bg_color)
+canvas.place(x=20, y=20)
+
+image = Image.open("HRHS_Logo.png").resize((100, 100))  # load the image from wikipedia and resize it
+photo_image = ImageTk.PhotoImage(image, width=100, height=100)
+canvas.create_image(50, 50, image=photo_image)
 
 # Add title
-title_label = tk.Label(root, text="HRHS CTE Partners", font=("Helvetica", 24, "bold"), fg="blue", background="white")
+title_label = tk.Label(root, text="HRHS CTE Partners", font=("Sylfaen", 24, "bold"), fg="blue", background=bg_color)
 title_label.place(x=140, y=20)
 
 # Add buttons
-readme_button = tk.Button(root, text="ReadMe", command=lambda: button_click("ReadMe"))
+readme_button = tk.Button(root, text="ReadMe", command=lambda: button_click("ReadMe"), background=button_color)
 readme_button.place(x=20, y=140)
 
-partners_button = tk.Button(root, text="Existing Partners", command=lambda: button_click("Existing Partners"))
+partners_button = tk.Button(root, text="Existing Partners", command=lambda: button_click("Existing Partners"), background=button_color)
 partners_button.place(x=20, y=186)
 
-documentation_button = tk.Button(root, text="Documentation", command=lambda: button_click("Documentation"))
+documentation_button = tk.Button(root, text="Documentation", command=lambda: button_click("Documentation"), background=button_color)
 documentation_button.place(x=20, y=232)
 
 # Add credits
-credits_label = tk.Label(root, text="Program Designed in Python 3.12 for FBLA by Alexander Fiduccia and Joe Hopkins", font=("Arial", 7), background="white")
-credits_label.place(x=54, y=278)
+credits_label = tk.Label(root, text="Program Designed in Python 3.12 for FBLA by Alexander Fiduccia and Joe Hopkins", font=("Arial", 7), background=bg_color)
+credits_label.place(x=48.5, y=278)
 
 # Start the GUI event loop
 root.mainloop()
